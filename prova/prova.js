e1ab64509d6cf2dd6e32c425571e340ffab3f9c9
Francois Lampasona <lampasonafrancois@gmail.com>
2024-11-19 20:47:46 +0100

    SplashPageCarousel Bs5

---
+++ prova/prova.js
@@ -0,0 +1,11 @@
+const login = function () {
+  const loginModal = new bootstrap.Modal(document.getElementById("loginModal"));
+  loginModal.show();
+};
+
+const register = function () {
+  const registerModal = new bootstrap.Modal(
+    document.getElementById("registerModal")
+  );
+  registerModal.show();
+};
