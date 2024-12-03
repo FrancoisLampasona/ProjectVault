f2278d522216a7176647e73f131e5d647bc39db4
Francois Lampasona <lampasonafrancois@gmail.com>
2024-12-03 23:19:51 +0100

    WeatherApp

--- WeatherApp/assets/js/script.js
+++ WeatherApp/assets/js/script.js
@@ -1,1 +1,7 @@
 //API OPEN WEATHER
+
+function prendiCitta() {
+  const nameCity = document.getElementById("nome-city"); // Seleziona l'input
+  const nome = nameCity.value; // Ottieni il valore dell'input
+  console.log(`La città inserita è: ${nome}`); // Stampa il valore nella console
+}
